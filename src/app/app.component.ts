da28d2123517959c50b6158c23f5582d369e8a3f
Fabian Axel Yañez Santa Cruz <faxyasan86@gmail.com>
2024-11-11 20:12:43 -0600

    commit examen practico 2do parcial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TemapComponent } from './tem/temap/temap.component';
+import { CapturaPedidoComponent } from './pizzas/captura-pedido/captura-pedido.component';
+import { DetallePedidoComponent } from './pizzas/detalle-pedido/detalle-pedido.component';
+import { VentasDiaComponent } from './pizzas/ventas-dia/ventas-dia.component';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, TemapComponent,],
+  imports: [RouterOutlet, TemapComponent, CapturaPedidoComponent, DetallePedidoComponent, VentasDiaComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/utl/proyectoapi.service.ts
@@ -0,0 +1,54 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { AlumnosUtl } from './interfaces/alumnosutl';
+ 
+@Pipe({
+  name: 'alumnoFilter',
+  standalone: true
+})
+export class AlumnoFilterPipe implements PipeTransform {
+ 
+  transform(value: AlumnosUtl[], args: string): AlumnosUtl[] {
+    let filter:string=args ?args.toLocaleLowerCase():'';
+ 
+    return filter? value.filter((alumno:AlumnosUtl)=>
+    alumno.nombre.toLocaleLowerCase().indexOf(filter)!=-1
+    ):value;
+  }
+ 
+}
+//import { HttpClient } from '@angular/common/http';
+import { HttpClient} from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { AlumnosUtl } from './interfaces/alumnosutl';
+import { Observable } from 'rxjs';
+ 
+@Injectable({
+  providedIn: 'root'
+})
+export class ProyectoapiService {
+ 
+  constructor(private  http: HttpClient) { }
+ 
+ 
+public getAlumnos():Observable<AlumnosUtl[]>{
+  return this.http.get<AlumnosUtl[]>('http://127.0.0.1:5000/alumnos')
+}
+ 
+agregarNuevoAlumno(datos:AlumnosUtl){
+  return this.http.post('http://127.0.0.1:5000/alumnos',datos)
+}
+ 
+public getAlumno(mat:number):Observable<AlumnosUtl>{
+  console.log(mat)
+  return this.http.get<AlumnosUtl>('http://127.0.0.1:5000/alumnos/'+mat)
+}
+ 
+modificarAlumno(mat:number,datos:AlumnosUtl){
+  return this.http.put('http://127.0.0.1:5000/alumnos/'+mat,datos)
+}
+public EliminaAlumno(mat:number):Observable<AlumnosUtl>{
+  return this.http.delete<AlumnosUtl>('http://127.0.0.1:5000/alumnos/'+mat)
+}
+ 
+}
+ 
